3e7bd15575338829f84b134481babd9e55c47eaa
seunghoonL <dltmdgns4029@gmail.com>
2023-11-22 22:32:31 +0900

    #11 의존성 업데이트 jpa + mysql  및 CRUD 테스트
    @DatajpaTest 사용법을 몰라 우선 h2 의존성은 삭제하였음

--- src/main/java/com/fastcampus/projectboard/domain/Article.java
+++ src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -1,17 +1,81 @@
 package com.fastcampus.projectboard.domain;
 
+import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@ToString
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "article")
 public class Article {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+
+    @Setter @Column(nullable = false)
     private String title; // 제목
+
+    @Setter @Column(length = 10000, nullable = false)
     private String content; // 본문
+
+    @Setter
     private String hashtag; // 해시태그
 
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+    @OrderBy("id")
+    @ToString.Exclude
+    private final Set<ArticleComment> articleComments = new HashSet<>();
 
+
+
+
+    @CreatedDate @Column(nullable = false)
     private LocalDateTime createdAt; // 생성일시
+    @CreatedBy @Column(length = 100, nullable = false)
     private String createdBy; // 생성자
+    @LastModifiedDate @Column(nullable = false)
     private LocalDateTime modifiedAt; // 수정일시
+    @LastModifiedBy @Column(length = 100, nullable = false)
     private String modifiedBy; // 수정자
+
+
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article)) return false;
+        Article article = (Article) o;
+        return id != null && Objects.equals(id, article.id);
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
+++ src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
@@ -1,18 +1,70 @@
 package com.fastcampus.projectboard.domain;
 
-import java.time.LocalDateTime;
+import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.Optional;
+
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "article_comment")
 public class ArticleComment {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+
+    @Setter @ManyToOne(optional = false)
     private Article article; // Article Id
+
+    @Setter @Column(length = 500, nullable = false)
     private String content; // 본문
 
 
+
+
+    @CreatedDate @Column(nullable = false)
     private LocalDateTime createdAt; // 생성일시
+    @CreatedBy  @Column(length = 100, nullable = false)
     private String createdBy; // 생성자
+    @LastModifiedDate @Column(nullable = false)
     private LocalDateTime modifiedAt; // 수정일시
+    @LastModifiedBy @Column(length = 100, nullable = false)
     private String modifiedBy; // 수정자
 
 
+
+    private ArticleComment(Article article, String content) {
+        this.article = article;
+        this.content = content;
+    }
+
+
+    public static ArticleComment of(Article article, String content){
+        return new ArticleComment(article, content);
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ArticleComment)) return false;
+        ArticleComment that = (ArticleComment) o;
+        return id != null && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
